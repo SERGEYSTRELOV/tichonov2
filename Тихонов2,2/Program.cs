27d4d2307fec779070ba851a4e3951618d0751b4
sergeystrelov <strelov09@mail.ru>
2023-08-07 21:23:47 +0300

    задача нахождение максимального числа из трёх

---
+++ Тихонов2,2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if((number1 > number2) && (number1 > number3))
+Console.WriteLine(number1);
+else
+    if(number2 > number3)
+    Console.WriteLine(number2);
+    else
+    {
+    Console.WriteLine(number3);
+    }
+
+
+

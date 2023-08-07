d6924379a228c9d3a5e007ad8923e36a18dc5814
sergeystrelov <strelov09@mail.ru>
2023-08-07 18:15:55 +0300

    Задача нахождения максимального из двух чисел

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу,которая на вход принимает два числа и выдает,какое число больше,а какое меньше.
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine (number1);
+}
+else
+{
+    Console.WriteLine (number2);
+}   
+ 
+
+
+
